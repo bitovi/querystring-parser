948bd7fcc114bc3256aa080968e36032558d0a36
Michael Haynie <mhaynie@bitovi.com>
2022-05-27 17:24:50 -0700

    gh-10: fix tests for new filter styles

--- test/helpers/determine-filter-style.test.js
+++ test/helpers/determine-filter-style.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable jest/expect-expect */
-const { determineFilterStyle } = require('../../lib/helpers/determine-filter-style')
+const determineFilterStyle = require('../../lib/helpers/determine-filter-style')
 const FilterStyle = require('../../lib/enums/filter-style')
 
 describe('determineFilterStyle', () => {
