a38e214d6338ec32501def2f1de720e0d1be315d
Michael Haynie <mhaynie@bitovi.com>
2022-06-02 14:34:14 -0700

    gh-13: update parseFields and parseInclude tests

--- test/parse-include.test.js
+++ test/parse-include.test.js
@@ -1,63 +1,46 @@
 const parseInclude = require("../lib/parse-include");
+const expectErrorsToMatch = require("./test-utils/expect-errors-to-match");
 
-describe("parseInclude", () => {
-  const testCases = [
-    {
-      title: "should return an array of relationship paths",
-      querystring:
-        "include=children.movies.actors.children,children.movies.actors.pets,children.pets,pets",
-      expectedResults: [
-        "children.movies.actors.children",
-        "children.movies.actors.pets",
-        "children.pets",
-        "pets",
-      ],
-    },
-
-    /**
-     * Alternate querystring formats
-     */
-    // TODO check for stuff like '?page=1' instead of '?page[number]=1
-    // do this for the other things too like filter, fields, etc
-
-    /**
-     * Query String Missing
-     */
-    {
-      title: "should return empty array when querystring is empty string",
-      querystring: "",
-      expectedResults: [],
-    },
-    {
-      title: "should return empty array when querystring is null",
-      querystring: null,
-      expectedResults: [],
-    },
-    {
-      title: "should return empty array when querystring is undefined",
-      querystring: undefined,
-      expectedResults: [],
-    },
-
-    /**
-     * Errors
-     */
-    // {
-    //   title: 'should return error if number was provided but size was not',
-    //   querystring: 'page[number]=1',
-    //   expectedResults: {},
-    //   expectedErrors: [
-    //     new Error('Page number was provided but page size was not in querystring: \'page[number]=1\'')
-    //   ]
-    // },
-  ];
-
+function testEachCase(testCases) {
   test.concurrent.each(testCases)(
     "$title",
     ({ querystring, expectedResults, expectedErrors }) => {
       const { results, errors } = parseInclude(querystring);
-      expect(results).toEqual(expectedResults);
-      expect(errors).toEqual(expectedErrors || []);
+      expect(results).toEqual(expectedResults || []);
+      expectErrorsToMatch(errors, expectedErrors || []);
     }
   );
+}
+
+describe("parseInclude", () => {
+  describe("parseInclude happy paths", () => {
+    testEachCase([
+      {
+        title: "should return an array of relationship paths",
+        querystring:
+          "include=children.movies.actors.children,children.movies.actors.pets,children.pets,pets",
+        expectedResults: [
+          "children.movies.actors.children",
+          "children.movies.actors.pets",
+          "children.pets",
+          "pets",
+        ],
+      },
+      {
+        title: "should return empty array when querystring is empty string",
+        querystring: "",
+        expectedResults: [],
+      },
+      {
+        title: "should return empty array when querystring is null",
+        querystring: null,
+        expectedResults: [],
+      },
+      {
+        title: "should return empty array when querystring is undefined",
+        querystring: undefined,
+        expectedResults: [],
+      },
+    ]);
+  });
 });
