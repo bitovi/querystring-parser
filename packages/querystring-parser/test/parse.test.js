841f82a2b8e18a7fcf5fb2dd05d00be3e40daee2
Roy <roylayindeay0@gmail.com>
2022-07-29 22:14:16 +0100

    added the tests back

--- packages/querystring-parser/test/parse.test.js
+++ packages/querystring-parser/test/parse.test.js
@@ -1,7 +1,75 @@
-// const parse = require('../lib/parse')
+const parse = require("../lib/parse");
+
+/** High level sanity tests */
 
 describe("parse", function () {
-  it("should run", function () {
-    expect("TODO: implement tests for this").toBeDefined();
+  it("should return results for multiple query parameters (mongo filter)", function () {
+    const querystring =
+      "sort=-date,name&page[number]=1&page[size]=5&include=pets&fields[people]=name&filter[name]=jay";
+    expect(parse(querystring)).toEqual({
+      filter: {
+        LIKE: ["#name", "%jay%"],
+      },
+      fields: {
+        people: ["name"],
+      },
+      page: {
+        number: 1,
+        size: 5,
+      },
+      sort: [
+        {
+          field: "date",
+          direction: "DESC",
+        },
+        {
+          field: "name",
+          direction: "ASC",
+        },
+      ],
+      include: ["pets"],
+      errors: {
+        filter: [],
+        fields: [],
+        page: [],
+        sort: [],
+        include: [],
+      },
+    });
+  });
+
+  it("should return results for multiple query parameters (ibm filter)", function () {
+    const querystring =
+      "sort=-date,name&page[number]=1&page[size]=5&include=pets&fields[people]=name&filter=contains(name,'jay')";
+    expect(parse(querystring)).toEqual({
+      filter: {
+        LIKE: ["#name", "%jay%"],
+      },
+      fields: {
+        people: ["name"],
+      },
+      page: {
+        number: 1,
+        size: 5,
+      },
+      sort: [
+        {
+          field: "date",
+          direction: "DESC",
+        },
+        {
+          field: "name",
+          direction: "ASC",
+        },
+      ],
+      include: ["pets"],
+      errors: {
+        filter: [],
+        fields: [],
+        page: [],
+        sort: [],
+        include: [],
+      },
+    });
   });
 });
