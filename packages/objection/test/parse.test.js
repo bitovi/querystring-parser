d65e63851abe906c8ebbe5dc6502d4e30cc44bc0
Roy <roylayindeay0@gmail.com>
2022-07-29 09:34:38 +0100

    Removed the licence in the sub-packages, the monorepo accounts for the licence for all

--- packages/objection/test/parse.test.js
+++
@@ -1,7 +0,0 @@
-// const parse = require('../lib/parse')
-
-describe("parse", function () {
-  it("should run", function () {
-    expect("TODO: implement tests for this").toBeDefined();
-  });
-});
