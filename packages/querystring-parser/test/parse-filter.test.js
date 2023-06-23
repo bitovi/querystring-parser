3081dc157a985dba38f80f96edfc726cd8e48f7f
Roye <roye@bitovi.com>
2023-06-23 09:09:31 -0400

    HATCH-176: Fix gte and lte

--- packages/querystring-parser/test/parse-filter.test.js
+++ packages/querystring-parser/test/parse-filter.test.js
@@ -17,34 +17,74 @@ describe("parseFilter", () => {
   describe("parseFilter happy paths", () => {
     testEachCase([
       {
-        title: "should parse strings correctly",
+        title: "should parse strings correctly (Mongo)",
         querystring: "filter[name]=kevin",
         expectedResults: { LIKE: ["#name", "%kevin%"] },
       },
       {
-        title: "should parse numbers correctly",
+        title: "should parse numbers correctly (Mongo)",
         querystring: "filter[age]=3",
         expectedResults: { "=": ["#age", 3] },
       },
       {
-        title: "should parse booleans correctly",
+        title: "should parse booleans correctly (Mongo)",
         querystring: "filter[active]=true",
         expectedResults: { "=": ["#active", true] },
       },
       {
-        title: "should parse strings correctly",
+        title: "should parse $lt correctly (Mongo)",
+        querystring: "filter[age][$lt]=3",
+        expectedResults: { "<": ["#age", 3] },
+      },
+      {
+        title: "should parse $lte correctly (Mongo)",
+        querystring: "filter[age][$lte]=3",
+        expectedResults: { "<=": ["#age", 3] },
+      },
+      {
+        title: "should parse $gt correctly (Mongo)",
+        querystring: "filter[age][$gt]=3",
+        expectedResults: { ">": ["#age", 3] },
+      },
+      {
+        title: "should parse $gte correctly (Mongo)",
+        querystring: "filter[age][$gte]=3",
+        expectedResults: { ">=": ["#age", 3] },
+      },
+      {
+        title: "should parse strings correctly (IBM)",
         querystring: "filter=contains(name,'kevin')",
         expectedResults: { LIKE: ["#name", "%kevin%"] },
       },
       {
-        title: "should parse numbers correctly",
+        title: "should parse numbers correctly (IBM)",
         querystring: "filter=equals(age,'3')",
         expectedResults: { "=": ["#age", 3] },
       },
       {
-        title: "should parse booleans correctly",
+        title: "should parse booleans correctly (IBM)",
         querystring: "filter=equals(active,'true')",
         expectedResults: { "=": ["#active", true] },
+      },
+      {
+        title: "should parse $lt correctly (IBM)",
+        querystring: "filter=lessThan(age,'3')",
+        expectedResults: { "<": ["#age", 3] },
+      },
+      {
+        title: "should parse $lte correctly (IBM)",
+        querystring: "filter=lessOrEqual(age,'3')",
+        expectedResults: { "<=": ["#age", 3] },
+      },
+      {
+        title: "should parse $gt correctly (IBM)",
+        querystring: "filter=greaterThan(age,'3')",
+        expectedResults: { ">": ["#age", 3] },
+      },
+      {
+        title: "should parse $gte correctly (IBM)",
+        querystring: "filter=greaterOrEqual(age,'3')",
+        expectedResults: { ">=": ["#age", 3] },
       },
       {
         title:
